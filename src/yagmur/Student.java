d5f69f194c2e8240c299c29998cfbab30c266885
yagmurmmmdova <yagmurmmmdova@gmail.com>
2026-08-25 20:05:43 +0400

    add Student class and implement all required methods and lambdas

---
+++ src/yagmur/Student.java
@@ -0,0 +1,59 @@
+package yagmur;
+
+import java.util.List;
+
+public class Student {
+    private int id;
+    private String name;
+    private double gpa;
+    private List<String> subjects;
+
+    public Student(int id, String name, double gpa, List<String> subjects) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+        this.subjects = subjects;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gpa=" + gpa +
+                ", subjects=" + subjects +
+                '}';
+    }
+}
